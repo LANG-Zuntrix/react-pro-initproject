8a5327b3c5c681d3946840dc05d45644afc22223
LuisMoov <lang@zuntrix.com>
2023-01-05 15:20:33 -0500

    Terminada sección de formularios dinámicos

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,13 +1,39 @@
 import { Formik, Form } from "formik";
 import formJson from '../data/custom-form.json';
-import { MyTextInput } from "../components";
+import { MySelect, MyTextInput } from "../components";
+import * as Yup from 'yup';
 
 const initialValues: { [x: string] : any } = {};
 
+const requiredFields: { [key: string] : any } = {};
+
+
 for ( const input of formJson ) {
     initialValues[ input.name ] = input.value;
+
+    if ( !input.validaions?.length ) continue;
+
+    let schema = Yup.string();
+
+    for (const rule of input.validaions) {
+        if ( rule.type === 'required' ) {
+            schema = schema.required( rule.desc );
+        }
+
+        if ( rule.type === 'minLength'){
+            schema = schema.min( (rule as any).value || 2, `${ rule.desc } ${ (rule as any).value || 2 }`);
+        }
+
+        if ( rule.type === 'email'){
+            schema = schema.email( rule.desc );
+        }
+        // TODO Otras reglas
+    }
+
+    requiredFields[input.name] = schema;
 }
 
+const validationSchema = Yup.object({ ...requiredFields });
 
 export const DynamicForm = () => {
     
@@ -19,6 +45,7 @@ export const DynamicForm = () => {
 
         <Formik
             initialValues={ initialValues }
+            validationSchema={ validationSchema }
             onSubmit={ (values) => {
                 console.log( values )
             }}
@@ -26,13 +53,30 @@ export const DynamicForm = () => {
 
             { (formik) => (
                 <Form noValidate>
-                    { formJson.map( ({ type, name, placeholder, label }) => {
-                        return <MyTextInput
-                                    key={ name }
-                                    type={ (type as any) }
-                                    name={ name }
-                                    label={ label }
-                                    placeholder={ placeholder } />
+                    { formJson.map( ({ type, name, placeholder, label, options }) => {
+
+                        if ( type === 'input' || type === 'password' || type === 'email' ) {
+
+                            return <MyTextInput
+                                        key={ name }
+                                        type={ (type as any) }
+                                        name={ name }
+                                        label={ label }
+                                        placeholder={ placeholder } />
+                        } else if ( type === 'select' ) {
+                            return (
+                                <MySelect key= { name } label={ label } name={ name }                                >
+                                    <option value="">Select an Option</option>
+                                    {
+                                        options?.map( opt => (
+                                            <option key={ opt.id } value={ opt.id } >{ opt.label }</option>
+                                        ))
+                                    }
+                                </MySelect>
+                            )
+                        }
+
+                        throw new Error (`El type: ${ type }, no es soportado`);
                     })}
                     <button type="submit" >Submit</button>
                 </Form>
